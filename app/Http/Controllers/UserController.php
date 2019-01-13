14d087650ed38e1f5270bd2177451d07d3ac7ab7
Luciano S. Junior <luciano.ssj@hotmail.com>
2019-01-13 15:41:10 -0200

    Criação do Model e Controller de Usuário. / Creation of User Model and Controller.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App/User; //importing model...
+
+class UserController extends Controller
+{
+    
+    public function index(){
+
+        $users = User::all();
+        $total = User::all()->count();
+
+        return view('list-users', compact('users', 'total'));
+
+    }
+
+    public function store(Request $request){
+
+        $user = new User();
+        $user->name     = $request->name;
+        $user->username = $request->username;
+        $user->password = $request->password;
+
+        $user->save();
+
+        return redirect()->route('user.index')->with('message', 'Usuário criado com sucesso!');
+
+    }
+
+    public function show($id){
+
+        //
+
+    }
+
+    public function edit($id){
+        
+        $user = User::findOrFail($id);
+
+        return view('edit-user', compact('user'));
+
+    }
+
+    public function update(Request $request, $id){
+
+        $user = User::findOrFail($id);
+
+        $user->name     = $request->name;
+        $user->username = $request->username;
+        $user->password = $request->password;
+
+        $user->save();
+
+        return redirect()->route('user.index')->with('message', 'Usuário alterado com sucesso!');
+
+    }
+
+    public function destroy($id){
+
+        $user = User::findOrFail($id);
+        
+
+    }
+
+}
